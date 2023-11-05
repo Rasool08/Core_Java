b82141a84d2e82da7f4c28226e770db0430a4c31
rasool08 <rasoolparuchuri@gmail.com>
2023-11-05 20:06:38 +0530

    Recursive method to find permutations of a string

--- Dupstr.java
+++ Dupstr.java
@@ -23,8 +23,9 @@ class Dupstr {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+	System.out.println("Enter a string:");
         String s = in.next();  // Read a string from the user
-	System.out.println(Enter a string:);
+	
         // Call the permutation method with the input string and an empty initial answer
         permutation(s, "");  // Function with two arguments without return
     }
